0a2f24176f2023485f075cc1726860306595b9bb
msadboy <ademaulana3427@gmail.com>
2023-02-28 06:21:46 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<div class="card">
+<div class="card-body">
+    <div class="row">
+        <div class="col-lg-6 p-4">
+            <h4></h4>
+            <p class="text-secondary">
+yy
+            </p>
+<a href="" class="btn btn-primary">Beri pengaduan</a>
+        </div>
+<div class="col-lg-6 btn-center">
+<img src="" width="200px" alt="">
+</div>
+    </div>
+
+</div>
+</div>
